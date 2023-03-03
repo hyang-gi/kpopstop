e60831946677316e51ff14fa1f8459de21e35b10
Han Yawei <hanyawei@Hans-MacBook-Air.local>
2023-03-03 21:12:02 +0000

    Added js files for loading building data

---
+++ pages/buildings/gallery.js
@@ -0,0 +1,18 @@
+var gallery = {
+  hybe: {
+    imageSrcs: [],
+    text: [],
+  },
+  yg: {
+    imageSrcs: [],
+    text: [],
+  },
+  sm: {
+    imageSrcs: [],
+    text: [],
+  },
+  jyp: {
+    imageSrcs: [],
+    text: [],
+  },
+};
---
+++ pages/buildings/history.js
@@ -0,0 +1,34 @@
+var history = {
+  hybe: {
+    upperText: "Lorem ipsum",
+    middleText: "Lorem ipsum",
+    lowerText: "Lorem ipsum",
+    uppperImageSrc: "",
+    middleImageSrc: "",
+    lowerImageSrc: "",
+  },
+  yg: {
+    upperText: "Lorem ipsum",
+    middleText: "Lorem ipsum",
+    lowerText: "Lorem ipsum",
+    uppperImageSrc: "",
+    middleImageSrc: "",
+    lowerImageSrc: "",
+  },
+  sm: {
+    upperText: "Lorem ipsum",
+    middleText: "Lorem ipsum",
+    lowerText: "Lorem ipsum",
+    uppperImageSrc: "",
+    middleImageSrc: "",
+    lowerImageSrc: "",
+  },
+  jyp: {
+    upperText: "Lorem ipsum",
+    middleText: "Lorem ipsum",
+    lowerText: "Lorem ipsum",
+    uppperImageSrc: "",
+    middleImageSrc: "",
+    lowerImageSrc: "",
+  },
+};
