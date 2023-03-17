5ab6781d1cbe7d3e4ebae16b86c58a7f376a7dfb
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2023-03-17 09:53:56 +0000

    corrected debug console statement

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
         console.log("header load performed");
     });
     $( ".footer" ).load( "../templates.html #footer", function(content) {
-        console.log("header load performed");
+        console.log("footer load performed");
     });
 });
