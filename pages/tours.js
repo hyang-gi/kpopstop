267b1dd0738cbcbd00d250044dced27f18ee8710
Han Yawei <hanyawei@Hans-MacBook-Air.local>
2023-03-18 20:11:12 +0000

    add tours.js

---
+++ pages/tours.js
@@ -0,0 +1,66 @@
+const HYBE=0,
+JYP=1,
+SM=2,
+YG=3;
+const tours=[
+    {
+      link: 'https://www.viator.com/tours/Seoul/Private-Tour-to-BTS-Film-Locations-in-Seoul/d973-192016P7',
+      agency: [HYBE],
+      title:"Private BTS Location Tour around Seoul City"
+    },
+    {
+      link: 'https://www.viator.com/tours/Seoul/1-Day-K-Pop-Tour-in-Seoul/d973-14882P39',
+      agency: [HYBE],
+      title:"BTS Tour with optional 1 day Dance Class"
+    },
+    {
+      link: 'https://www.indiway.com/en/prod/seoul-kpop-fan-tour',
+      agency: [JYP],
+      title:"Seoul Hallyu K-pop Fan Tour"
+    },
+    {
+      link: 'https://www.viator.com/tours/Seoul/Private-Tour-Discover-the-Korean-Wave-in-Gangnam/d973-15527P8',
+      agency: [SM],
+      title:"Private Tour: Discover the Korean Wave in Gangnam"
+    },
+    {
+      link: 'https://www.viator.com/tours/Seoul/Private-A-Tour-Guide-for-K-Pop-and-K-Drama-Hallyu-Tour-in-Gangnam/d973-139289P183',
+      agency: [SM],
+      title:"Private A Tour Guide for K-Pop and K-Drama Hallyu Tour in Gangnam"
+    },
+    {
+      link: 'https://www.trazy.com/experience/detail/smtown-studio-tour-coex-smtown-artium-seoul',
+      agency: [SM],
+      title:"SMTOWN Studio Tour & COEX SMTOWN Artium"
+    },
+    {
+      link: 'https://www.triphobo.com/places/seoul-south-korea/yg-entertainment',
+      agency: [YG],
+      title:"YG Entertainment"
+    },
+    {
+      link: 'https://www.koreaetour.com/product/1-day-k-pop-star-tour-private-tour/',
+      agency: [YG,SM],
+      title:"1 Day K-Pop Star Tour (Private Tour)"
+    },
+    {
+      link: 'https://iamyourguide.net/product/full-day-private-tour-of-k-pop-k-wave-experience/',
+      agency: [YG,SM,HYBE,JYP],
+        title:"Full Day Private Tour of K-Pop & K-Wave Experience"
+    },
+    {
+      link: 'https://www.trazy.com/experience/detail/kpop-entertainment-idol-tour-seoul',
+      agency: [YG,SM,HYBE,JYP],
+        title:"K-Pop Entertainment Companies & Idol Restaurant Private Tour"
+    },
+    {
+      link: 'https://tntkoreatravel.com/tour/kpop-tour-kdrama-tour/?ckattempt=1',
+      agency: [YG,SM,JYP],
+      title:"BTS Tour with optional 1 day Dance Class"
+    },
+    {
+      link: 'https://18616.partner.viator.com/tours/Seoul/K-pop-Agencies-and-Idol-Restaurant-Visit-in-Seoul-with-Guide-and-Transport/d973-139289P157?SUBPUID=T8EYirv2',
+      agency: [YG,SM,HYBE,JYP],
+      title:"K-pop Agencies and Idol Restaurant Visit in Seoul with Guide & Transport"
+    }
+  ];
